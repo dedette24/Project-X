801f72110a282032386546a69537a3573a0fd9c2
dedette24 <77748549+dedette24@users.noreply.github.com>
2023-11-20 09:41:39 +0100

    Add files via upload

---
+++ Poopkemon.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+
+class Pokemon:
+    def __init__(self):
+        self.nom = "Son blaze"
+        self.vie = int("Vie") #entre 78 et 150
+        self.dfs = int("Attaque") #entre 5 et 20
+        self.type = "son type (pour savoir si il fait degat *2)" #10 type : Electrique, Feu, Glace, Eau, Terre, Gadou, Plante, Ombre, caca, radiateur
+        self.atq = {} #entre 2 et 4
+        
+Pigrochou = Pokemon()
+Pigrochou.nom = "Prigrochou"
+Pigrochou.vie = "100"
+Pigrochou.dfs = "5"
+Pigrochou.type = "Electrique"
+Pigrochou.atq = {"Coup de foudre" : int("20"), "Coup de queue" : int("10")}
