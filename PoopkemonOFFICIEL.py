6681aa4a6f3fa3c90b7417d2e111b6e1ae025ce1
Eliasezz <147371589+Eliasezz@users.noreply.github.com>
2023-11-21 16:21:17 +0100

    Update PoopkemonOFFICIEL.py
    Fini pokemon

--- PoopkemonOFFICIEL.py
+++ PoopkemonOFFICIEL.py
@@ -34,6 +34,112 @@ ciseau.type = "acier"
 ciseau.atq = {"tranchant" : int("15"), "ca coupe..." : int("20")}
 liste_tt_pokemon.append(ciseau)
 
+Torpingpong = Pokemon()
+Torpingpong.nom = Torpingpong
+Torpingpong.vie = int("140")
+Torpingpong.dfs = int("15")
+Torpingpong.type = "Eau"
+Torpingpong.atq = {"Pistolet à eau " : int("15"), "Hydrocanon" : int("12")}
+
+Zegrouille = Pokemon()
+Zegrouille.nom = Zegrouille
+Zegrouille.vie = int("100")
+Zegrouille.dfs = int("10")
+Zegrouille.type = "Radiateur"
+Zegrouille.atq = {" Rayon Chargé" : int("20"), "Éclair Fou" : int("14")}
+
+Terramen = Pokemon()
+Terramen.nom = Terramen
+Terramen.vie = int("100")
+Terramen.dfs = int("25")
+Terramen.type = "Terre"
+Terramen.atq = {" Ecra-Terre" : int("15"), " Tremblement de ramen" : int("25")}
+
+sarbouboule = Pokemon()
+sarbouboule.nom =sarbouboule
+sarbouboule.vie = int("100")
+sarbouboule.dfs = int("25")
+sarbouboule.type = "Glace"
+sarbouboule.atq = {" Attaque glaçon" : int("15"), " freezer" : int("25")}
+
+
+arkekes = Pokemon()
+arkekes.nom = arkekes
+arkekes.vie = int("100")
+arkekes.dfs = int("24")
+arkekes.type = "Ombre"
+arkekes.atq = {"Noir-sombre" : int("15"), " blindness" : int("25")}
+
+
+Gagabou = Pokemon()
+Gagabou.nom = Gagabou
+Gagabou.vie = int("90")
+Gagabou.dfs = int("19")
+Gagabou.type = "Gadou"
+Gagabou.atq = {"Lame Feuille" : int("15"), "Colère Gaga" : int("25")}
+
+
+Taumamamia = Pokemon()
+Taumamamia.nom = Taumamamia
+Taumamamia.vie = int("100")
+Taumamamia.dfs = int("20")
+Taumamamia.type = "Feu"
+Taumamamia.atq = {"Centrifugifle" : int("15"), "Coup de corne extrême" : int("25")}
+
+
+Sologopitch = Pokemon()
+Sologopitch.nom = Sologopitch
+Sologopitch.vie = int("100")
+Sologopitch.dfs = int("10")
+Sologopitch.type = "caca"
+Sologopitch.atq =  {"Vibrocaca" : int("15"), "Tacle lourd diarrhée " : int("25")}
+
+
+Bulbaredbull = Pokemon ()
+Bulbaredbull.nom = Bulbaredbull
+Bulbaredbull.vie = int("100")
+Bulbaredbull.dfs = int("8")
+Bulbaredbull.type = "Eau"
+Bulbaredbull.atq = {"donne des ailes" : int("15"), "plume endormissant" : int("25")}
+
+
+charmandercharmant = Pokemon()
+charmandercharmant.nom = charmandercharmant
+charmandercharmant.vie = int("110")
+charmandercharmant.dfs = int("16")
+charmandercharmant.type = "caca"
+charmandercharmant.atq ={"la grâce suspecte" : int("15"), "lumière 100%" : int("25")}
+
+
+NeSquictle = Pokemon ()
+charmandercharmant.nom = 
+charmandercharmant.vie = int("99")
+charmandercharmant.dfs = int("19")
+charmandercharmant.type = "Terre"
+charmandercharmant.atq = {"Lumière charmantisme" : int("15"), "charismatique" : int("25")}
+
+JicleJacqueline = Pokemon ()
+JicleJacqueline.nom = JicleJacqueline
+JicleJacqueline.vie = int("100")
+JicleJacqueline.dfs = int("19")
+JicleJacqueline.type = "Plante"
+JicleJacqueline.atq = {"Gifle ortie" : int("15"), "Lance-Soleil" : int("25")}
+
+Sorsdurelax =Pokemon ()
+Sorsdurelax.nom = Sorsdurelax
+Sorsdurelax.vie = int("100")
+Sorsdurelax.dfs = int("13")
+Sorsdurelax.type = "radiateur"
+Sorsdurelax.atq = {"nahass" : int("15"), "bulle piquante" : int("25")}
+
+Gytan = Pokemon()
+Gytan.nom = Gytan
+Gytan.vie = int("150")
+Gytan.dfs = int("20")
+Gytan.type = "Feu et Electrique"
+Gytan.atq = {"fusion power" : int("15"), "All power" : int("25")}
+
+
 def choix_random(liste_tt_pokemon):
     equipe_1 = []
     equipe_2 = []
