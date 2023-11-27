9b70e7a7bd969d9d901ef62ebf850e4af1deb1dd
dedette24 <77748549+dedette24@users.noreply.github.com>
2023-11-27 08:35:00 +0100

    Update PoopkemonOFFICIEL.py

--- PoopkemonOFFICIEL.py
+++ PoopkemonOFFICIEL.py
@@ -2,12 +2,11 @@ import random as rd
 import time
 
 class Pokemon:
-    def __init__(self): 
+    def __init__(self):
         self.nom = "Son blaze"
         self.vie = "Vie" #entre 78 et 150
         self.dfs = "Attaque" #entre 5 et 20
-        self.vit = "Vitesse" #entre 10 et 100
-        self.type = "son type (pour savoir si il fait degat *2)" #10 type : Caca, Feu, Eau, Gadou, Radiateur, Roche, Acier, Dragon, Glace, Ombre, emotionel
+        self.type = "son type (pour savoir si il fait degat *2)" #10 type : Electrique, Feu, Glace, Eau, Terre, Gadou, Plante, Ombre, acier, radiateur
         self.atq = {} #entre 2 et 4
         
 liste_tt_pokemon = []
@@ -17,7 +16,6 @@ Pigrochou.nom = "Pigrochou"
 Pigrochou.vie = int(100)
 Pigrochou.dfs = int(5)
 Pigrochou.type = "Electrique"
-Pigrochou.vit = int(10)
 Pigrochou.atq = {"Coup de foudre" : int("20"), "Coup de queue" : int("10")}
 liste_tt_pokemon.append(Pigrochou)
 #Pokemon num 2
@@ -145,7 +143,7 @@ Gytan = Pokemon()
 Gytan.nom = "Gytan"
 Gytan.vie = int("150")
 Gytan.dfs = int("20")
-Gytan.type = "Roche"
+Gytan.type = "Feu et Electrique"
 Gytan.atq = {"fusion power" : int("15"), "la pelle" : int("25")}
 liste_tt_pokemon.append(Gytan)
 
@@ -173,47 +171,71 @@ equipe_2_P1_type, equipe_2_P2_type = equipe_2[0].type, equipe_2[1].type
 equipe_2_P1_atq, equipe_2_P2_atq = equipe_2[0].atq, equipe_2[1].atq
 print(f"équipe 1 : {equipe_1[0].nom} et {equipe_1[1].nom} / equipe 2 : {equipe_2[0].nom} et {equipe_2[1].nom} ")
 
-def start():
+def start(): #qui commence (flemme de faire un truc long ducoup y'a aussi arrangement irl mais ca marche)
     print("l'un d'entre vous choisie un chiffre entre 1 et 2 si le chiffre choisie par la perosnne est le bon, alors il commence, sinon c'est l'autre.")
     commence = rd.randint(1,2)
     print("vous avez une minute pour vous décidez")
-    time.sleep(60)#1min
+    time.sleep(10)#1min
     print("roulement de tambourrrrr")
     if commence == 1:
         print("c'est le joueur 1 qui commence")
     else:
-        print("c'est le joueur 2 qui commence") 
+        print("c'est le joueur 2 qui commence")
 
-
-        
-def combat():
-    return
-
-def surrender():
+def surrender(): #si le joueur veut abandonner (marche ducoup on met entre crochet)
     abandon = int(input("tu ff ? si oui ecrit 1, sinon ecrit 2 : "))
     if abandon == 1:
         print("Le joueur machin a voté pour abandonner ! bien jouer autre joueur !")
-        combat = False
+        jeux = False
+        return jeux
     else: 
         print("la partie continue !")
         pass
 
-surrender()
+def fin_de_jeu_vie(equipe_1_P1_vie, equipe_1_P2_vie, equipe_2_P1_vie, equipe_2_P2_vie):
+    if equipe_1_P1_vie == 0 and equipe_1_P2_vie == 0 or equipe_2_P1_vie == 0 and equipe_2_P2_vie == 0:
+        jeux = False
+        print("la partie est fini !")
+        return jeux
+    else: 
+        print("la partie continue")
+        pass
 
+def choix_pokemon(equipe_1, equipe_2, tours): #celui qui joue decide quel pokemon jouer
+    if tours / 2 == type(float):
+        print(f"voci les pokemon que tu as : {equipe_1[0].nom} qui as {equipe_1[0].vie} et {equipe_1[1].nom} qui as {equipe_1[1].vie}")
+        joueur_1_pokemon_jouer = int(input(f"tu veux jouer qui ? 1 = {equipe_1[0].nom} ou 2 = {equipe_1[0].nom}: "))
+        if joueur_1_pokemon_jouer == 1:
+            joueur_1_pokemon_jouer = equipe_1[0]
+        else:
+            joueur_1_pokemon_jouer = equipe_1[1]
+        return joueur_1_pokemon_jouer
+    elif tours / 2 == type(int):
+        print(f"voci les pokemon que tu as : {equipe_2[0].nom} qui as {equipe_2[0].vie} et {equipe_2[1].nom} qui as {equipe_2[1].vie}")
+        joueur_2_pokemon_jouer = int(input(f"tu veux jouer qui ? 1 = {equipe_2[0].nom} ou 2 = {equipe_2[0].nom}: "))
+        if joueur_2_pokemon_jouer == 1:
+            joueur_2_pokemon_jouer = equipe_2[0]
+        else:
+            joueur_2_pokemon_jouer = equipe_2[1]
+        return joueur_2_pokemon_jouer
 
-
-def fin_de_combat(pokemon):
-    pokemon = pokemon
-    mort = sum([not pokemon.est_vivant for pokemon in pokemon])
-    if mort == 2:
-        return True
+def attaque_pokemon(liste_tt_pokemon, tours, joueur_1_pokemon_jouer, joueur_2_pokemon_jouer,equipe_1_P1_atq, equipe_1_P2_atq,equipe_2_P1_atq, equipe_2_P2_atq):
+    print("c'est l'heure d'attaquer !")
+    if tours / 2 == type(float):    
+        choix = input(f"t'attaque qui ? : 0 = {equipe_1[0].nom} ou 1 = {equipe_1[1].nom} ? : ")
+        pokemon_attaquer = equipe_1[choix]
+        choix2 = input(f"t'attaque qui ? : 0 = {equipe_2[0].nom} ou 1 = {equipe_2[1].nom} ? : ")
+        pokemon_attaquer = equipe_2[choix]
+        attaque = input(f"tu veux utiliser quel attaque joueur 1 ? 0 = {equipe_1_P1_atq}")
     else:
-        return False
+        choix = input(f"t'attaque qui ? : 0 = {equipe_2[0].nom} ou 1 = {equipe_2[1].nom} ? : ")
+        pokemon_attaquer = equipe_2[choix]
+        choix = input(f"t'attaque qui ? : 0 = {equipe_1[0].nom} ou 1 = {equipe_1[1].nom} ? : ")
+        pokemon_attaquer = equipe_1[choix]
+        
     
+    
+    
+    
+        
 
-#Nom de poopkemon:
-#Le dieu des poopkemon : Arc
-#Monsieur Porc
-#Roitiflame => Groroti
-#Revalio
-#
