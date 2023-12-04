6441b9386fefce7a6a1c315955a912e90b39ef9f
Eliasezz <147371589+Eliasezz@users.noreply.github.com>
2023-12-04 09:48:34 +0100

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,3 +1,12 @@
 import random as rd
 import MAIN
 import classe
+
+class Dodo()
+  def __init__(self):
+    self.fatigué = {self.dodo}
+
+def fatiguer(self, cible):
+        if self.fatigue > 0:
+            print(f"{self.nom} est fatigué et ne peut pas attaquer ce tour.Il vous reste ")
+            self.reposer()
