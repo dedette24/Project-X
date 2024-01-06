e32a83abab7ed17993c670c257c5abd4ea67580d
dedette24 <77748549+dedette24@users.noreply.github.com>
2024-01-06 11:46:47 +0100

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,12 +1,37 @@
-import random as rd
-import MAIN
+import random
 import classe
 
-class Dodo()
-  def __init__(self):
-    self.fatigué = {self.dodo}
+class TEST:
+  def __init__(self) -> None:
+    self.vitesse = "vts"
 
-def fatiguer(self, cible):
-        if self.fatigue > 0:
-            print(f"{self.nom} est fatigué et ne peut pas attaquer ce tour.Il vous reste ")
-            self.reposer()
+TEST1 = TEST()
+TEST1.vitesse = int(10)
+
+TEST2 = TEST()
+TEST2.vitesse = int(10)
+test = [1, 1]
+
+
+if test == [1, 1]:
+  if TEST1.vitesse < TEST2.vitesse:
+    print("1111111")
+  elif TEST1.vitesse == TEST2.vitesse:
+    print("on va jeter les dés !")
+    x = int(random.randint(1,2))
+    print(x)
+    if x == 1:
+      print("l'équipe 1 va commencé a attaquer en premier")
+      print("1, jcp, etvoila")
+    else:
+      print("l'équipe 2 va commencé a attaquer en premier")
+      print("2, jcp, etvoila")
+  else:
+    print("22222222")
+elif 1 in test:
+  coordonnee = test.index(1) + 1
+  print(coordonnee)
+  print("33333333333333333333")
+else:
+  print("444444444444444444")
+  pass
